8e5315ebad0bcd17ebb5ce9a9b857dfe15237665
GhassenAbida <92631158+GhassenAbida@users.noreply.github.com>
2023-12-04 20:45:09 +0100

    Update example.test.js

--- example.test.js
+++ example.test.js
@@ -1,4 +1,12 @@
-// example.test.js
-test('basic test', () => {
-  expect(1 + 1).toBe(2);
+const request = require('supertest');
+const app = require('./app'); // Import the Express app
+
+describe('GET /items', () => {
+  it('responds with JSON', async () => {
+    const response = await request(app).get('/items');
+    expect(response.statusCode).toBe(200);
+    expect(response.type).toBe('application/json');
+    // Add more assertions as needed, depending on your actual response structure
+  });
 });
+
